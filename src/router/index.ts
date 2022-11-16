1003907fb3d881c86316a36e05528438ed4f02c6
onurusluca <onur@marsflag.com>
2022-11-16 14:04:35 +0900

    query test 3.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import type { RouteRecordRaw } from "vue-router";
+import Home from "../views/HomeView.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/:contractId?",
+    name: "Home",
+    component: Home,
+  },
+
+  {
+    path: "/query:afterQuery(.*)",
+    component: () => import("../views/Query.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-
-    {
-      path: "/query:afterQuery(.*)",
-      component: () => import("../views/Query.vue"),
-    },
-  ],
+  routes, // short for `routes: routes`
 });
 
 export default router;
